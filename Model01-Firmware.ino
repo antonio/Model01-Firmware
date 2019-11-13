c36915eecf37f754de0917e408e0ab8f01aaf025
Antonio Santos <antonio@santosvelasco.com>
2019-11-13 10:56:17 +0100

    More layout customization

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -222,17 +222,17 @@ KEYMAPS(
 #elif defined (PRIMARY_KEYMAP_CUSTOM)
   // Edit this keymap to make a custom layout
   [PRIMARY] = KEYMAP_STACKED
-  (Key_Backtick    , Key_1         , Key_2      , Key_3        , Key_4, Key_5, Key_LEDEffectNext,
-   Key_Tab         , Key_Q         , Key_W      , Key_E        , Key_R, Key_T, Key_Tab          ,
+  (Key_Backtick    , Key_1         , Key_2      , Key_3        , Key_4, Key_5, Key_6      ,
+   Key_Tab         , Key_Q         , Key_W      , Key_E        , Key_R, Key_T, Key_LeftBracket    ,
    Key_PageUp      , Key_A         , Key_S      , Key_D        , Key_F, Key_G,
-   Key_LeftShift   , Key_Z         , Key_X      , Key_C        , Key_V, Key_B, Key_Escape       ,
+   Key_LeftShift   , Key_Z         , Key_X      , Key_C        , Key_V, Key_B, Key_Escape ,
    Key_LeftControl , Key_Backspace , Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY)   , Key_6      , Key_7       , Key_8           , Key_9        , Key_0        , LockLayer(NUMPAD),
-   Key_Enter      , Key_Y      , Key_U       , Key_I           , Key_O        , Key_P        , Key_Equals       ,
+   Key_7          , Key_8      , Key_9       , Key_0           , Key_Minus    , Key_Equals   , Key_Backspace,
+   Key_Enter      , Key_Y      , Key_U       , Key_I           , Key_O        , Key_P        , Key_Equals   ,
    Key_H          , Key_J      , Key_K       , Key_L           , Key_Semicolon, Key_Quote    ,
-   Key_RightAlt   , Key_N      , Key_M       , Key_Comma       , Key_Period   ,    Key_Slash , Key_Minus        ,
+   Key_RightAlt   , Key_N      , Key_M       , Key_Comma       , Key_Period   ,    Key_Slash , Key_Minus    ,
    Key_RightShift , Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 

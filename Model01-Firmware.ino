b2d10ceeedbc404b3070807ab53ab8e784e13fbd
Antonio Santos <antonio@santosvelasco.com>
2019-11-18 12:33:57 +0100

    Basic layout complete

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -222,18 +222,18 @@ KEYMAPS(
 #elif defined (PRIMARY_KEYMAP_CUSTOM)
   // Edit this keymap to make a custom layout
   [PRIMARY] = KEYMAP_STACKED
-  (Key_Backtick    , Key_1         , Key_2      , Key_3        , Key_4, Key_5, Key_6               ,
-   Key_Tab         , Key_Q         , Key_W      , Key_E        , Key_R, Key_T, Key_LeftBracket     ,
-   Key_Escape      , Key_A         , Key_S      , Key_D        , Key_F, Key_G,
-   Key_LeftShift   , Key_Z         , Key_X      , Key_C        , Key_V, Key_B, Key_KeypadLeftParen ,
-   Key_LeftControl , Key_Backspace , Key_LeftGui, Key_LeftShift,
+  (Key_Backtick  , Key_1      , Key_2    , Key_3 , Key_4, Key_5, Key_6               ,
+   Key_Tab       , Key_Q      , Key_W    , Key_E , Key_R, Key_T, Key_LeftBracket     ,
+   Key_Escape    , Key_A      , Key_S    , Key_D , Key_F, Key_G,
+   Key_LeftShift , Key_Z      , Key_X    , Key_C , Key_V, Key_B, Key_KeypadLeftParen ,
+   Key_LeftAlt   , Key_LeftGui, Key_Hyper, ___   , // Another function layer, for gaming?
    ShiftToLayer(FUNCTION),
 
-   Key_7                , Key_8      , Key_9       , Key_0           , Key_Minus    , Key_Equals   , Key_Backspace,
-   Key_RightBracket     , Key_Y      , Key_U       , Key_I           , Key_O        , Key_P        , Key_Equals   ,
-   Key_H                , Key_J      , Key_K       , Key_L           , Key_Semicolon, Key_Quote    ,
-   Key_KeypadRightParen , Key_N      , Key_M       , Key_Comma       , Key_Period   ,    Key_Slash , Key_Minus    ,
-   Key_RightShift       , Key_LeftAlt, Key_Spacebar, Key_RightControl,
+   Key_7                , Key_8      , Key_9       , Key_0           , Key_Minus    , Key_Equals       , Key_Backspace  ,
+   Key_RightBracket     , Key_Y      , Key_U       , Key_I           , Key_O        , Key_P            , Key_Quote      ,
+   Key_H                , Key_J      , Key_K       , Key_L           , Key_Semicolon, Key_RightControl ,
+   Key_KeypadRightParen , Key_N      , Key_M       , Key_Comma       , Key_Period   , Key_Slash        , Key_RightShift ,
+   ___                  , Key_Enter  , Key_Spacebar, ___,
    ShiftToLayer(FUNCTION)),
 
 #else
@@ -260,18 +260,18 @@ KEYMAPS(
    ___),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_CapsLock,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
-   ___, Key_Delete, ___, ___,
+  (___, Key_F1, Key_F2, Key_F3, Key_F4, Key_F5, Key_F6,
+   ___, ___   , ___   , ___   , ___   , ___   , ___   ,
+   ___, ___   , ___   , ___   , ___   , ___   ,
+   ___, ___   , ___   , ___   , ___   , ___   , ___   ,
+   ___, ___   , ___   , ___   ,
    ___,
 
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
-   ___, ___, Key_Enter, ___,
+   Key_F7                      , Key_F8       , Key_F9              , Key_F10              , Key_F11        , Key_F12         , ___,
+   ___                         , ___          , Key_LeftCurlyBracket, Key_RightCurlyBracket, Key_LeftBracket, Key_RightBracket, ___,
+                  Key_LeftArrow, Key_DownArrow, Key_UpArrow         , Key_RightArrow       ,  ___           , ___             ,
+   ___                         , ___          , ___                 , ___                  , ___            , ___             , ___,
+   ___                         , ___          , ___                 , ___                  ,
    ___)
 ) // KEYMAPS(
 
